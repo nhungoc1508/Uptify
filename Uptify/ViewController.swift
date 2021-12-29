f5132dce86384c75d0328f1e154b6e6a104a1c17
Ngoc N. Hoang <nnh245@nyu.edu>
2021-12-29 21:56:28 +0400

    Initial Commit

---
+++ Uptify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uptify
+//
+//  Created by Ngoc Hoang on 29/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
